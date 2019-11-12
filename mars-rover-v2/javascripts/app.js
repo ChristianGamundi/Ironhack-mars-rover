05ee9b840ae55eafe76d3fa3590c807f201efce3
Gamundi Christian <20138@columbushs.com>
2019-11-12 10:26:19 -0500

    MarsRover

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,15 +1,58 @@
-// Rover Object Goes Here
-// ======================
 
-// ======================
-function turnLeft(rover){
+let rover = {
+  direction: "N",
+ }
+
+function turnLeft(rover){  
   console.log("turnLeft was called!");
+
+if (rover.direction == "N"){
+  rover.direction = "W";
+}
+else if (rover.direction == "W"){
+ rover.direction = "S";
+}
+else if (rover.direction == "S"){
+  rover.direction = "E";
+}
+else{
+rover.direction = "N";
+}
+
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+
+  if (rover.direction == "N"){
+    rover.direction = "E";
+  }
+  else if (rover.direction == "E"){
+    rover.direction = "S";    
+  }
+  else if (rover.direction == "S"){
+    rover.direction = "W";
+   }
+   else{
+     rover.direction = "N";
+   }
 }
 
 function moveForward(rover){
-  console.log("moveForward was called")
-}
+  console.log("moveForward was called");
+  if (rover.direction == "N"){
+    rover.direction = "N";
+  }
+   else if (rover.direction == "S"){
+   rover.direction = "S";
+   }
+   else if (rover.direction == "W"){
+     rover.direction = "W";
+   }
+   else{
+     rover.direction = "E";
+   }
+  }
+
+
+
